d54e8fee78f155270cc2965fa3c82fbea71d5bd8
Rachan <86477482+RachanKaur@users.noreply.github.com>
2021-09-06 17:26:37 +0100

    Update pulsarClient.js

--- src/pulsarClient/pulsarClient.js
+++ src/pulsarClient/pulsarClient.js
@@ -1,6 +1,6 @@
 const Pulsar = require('pulsar-client')
 
-const tokenStr = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjbGllbnQ7NjAzYWY0MGYtMTZlNC00YjFjLWEyZDEtZDYwYmVkZjEwZmUwO2NtRmphR0Z1In0.YXJdy8xjnmxLgdid5D1uztjcJtLbWWaWCKq14wHsvd4Obfi09lc_vRdWgUZNd7DFLXawzCVyR6cCA80I7niJkZ8Ce62G_YKK4f6p0_S4FHL7FTi9_iphAYGGzgYIOCePjmzwnExDFoMfft5ISCTHqQrvULKoTJpIORSNmrKIJqlfO9mGcQAQ54HqDTAuJFVn129owGSdzuWheHWP2YenuTL18xTFRyJVFm0fo1U6Lfopxbodq5uHNl_I_WS4GzvqTP4ce9XjF874MJAG90arpS2jeAobqtIOcv0pab1Gu-BWgM52kn0rue2VuNnsJRvJa7C2W0NL8hgGukF3ZBYGjA";
+const tokenStr = "";
 const pulsarUri = "pulsar+ssl://pulsar-aws-useast2.streaming.datastax.com:6651";
 const topicName ="persistent://rachan/default/learn3";
 
